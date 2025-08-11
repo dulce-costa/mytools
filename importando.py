148a4c534e8f2df2572d04eae73d3df6cef282fe
dulce-costa <al.dulce.costa@impatech.org.br>
2025-08-11 16:13:31 -0300

    importando

---
+++ importando.py
@@ -0,0 +1,6 @@
+import MYTOOLS
+
+N_pi=int(input("Numero para a quantidade de casas decimais de pi:"))
+N_e=int(input("Numero para a quantidade de casas decimais de e:"))
+print("Número pi com",N_pi,"casas decimais:",MYTOOLS.pi_real(N_pi))
+print("Número pi com",N_e,"casas decimais:",MYTOOLS.e_real(N_e))
